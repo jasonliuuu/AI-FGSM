b6120c28eca9bddc37f79071aca7b6af2265446c
DazhiZhong <zhongdz.dazhi2018@gdhfi.com>
2020-07-26 23:03:07 +0800

    config + utils

---
+++ fgsmutils.py
@@ -0,0 +1,152 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import cv2
+import pandas as pd
+import scipy.stats as st
+from scipy.misc import imread, imsave
+from tensorflow.contrib.image import transform as images_transform
+from tensorflow.contrib.image import rotate as images_rotate
+
+import tensorflow as tf
+
+from nets import inception_v3, inception_v4, inception_resnet_v2, resnet_v2
+
+import random
+
+from config import *
+
+
+def load_images(input_dir, batch_shape):
+    """Read png images from input directory in batches.
+    Args:
+      input_dir: input directory
+      batch_shape: shape of minibatch array, i.e. [batch_size, height, width, 3]
+    Yields:
+      filenames: list file names without path of each image
+        Lenght of this list could be less than batch_size, in this case only
+        first few images of the result are elements of the minibatch.
+      images: array with all images from this batch
+    """
+    images = np.zeros(batch_shape)
+    filenames = []
+    idx = 0
+    batch_size = batch_shape[0]
+    for filepath in tf.gfile.Glob(os.path.join(input_dir, '*'))[STARTING_INDEX:END_INDEX]:
+        with tf.gfile.Open(filepath, 'rb') as f:
+            image = imread(f, mode='RGB').astype(np.float) / 255.0
+        # Images for inception classifier are normalized to be in [-1, 1] interval.
+        images[idx, :, :, :] = image * 2.0 - 1.0
+        filenames.append(os.path.basename(filepath))
+        idx += 1
+        if idx == batch_size:
+            yield filenames, images
+            filenames = []
+            images = np.zeros(batch_shape)
+            idx = 0
+    if idx > 0:
+        yield filenames, images
+
+
+def save_images(images, filenames, output_dir):
+    """Saves images to the output directory.
+
+    Args:
+        images: array with minibatch of images
+        filenames: list of filenames without path
+            If number of file names in this list less than number of images in
+            the minibatch then only first len(filenames) images will be saved.
+        output_dir: directory where to save images
+    """
+    for i, filename in enumerate(filenames):
+        # Images for inception classifier are normalized to be in [-1, 1] interval,
+        # so rescale them back to [0, 1].
+        with tf.gfile.Open(os.path.join(output_dir, filename), 'w') as f:
+            imsave(f, (images[i, :, :, :] + 1.0) * 0.5, format='png')
+
+
+def check_or_create_dir(directory):
+    """Check if directory exists otherwise create it."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def graph(x, y, i, x_max, x_min, grad):
+    eps = 2.0 * FLAGS.max_epsilon / 255.0
+    num_iter = FLAGS.num_iter
+    alpha = eps / num_iter
+    momentum = FLAGS.momentum
+    num_classes = 1001
+
+    with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
+        logits_v3, end_points_v3 = inception_v3.inception_v3(
+            x, num_classes=num_classes, is_training=False, reuse=tf.AUTO_REUSE)
+
+    pred = tf.argmax(end_points_v3['Predictions'], 1)
+
+    first_round = tf.cast(tf.equal(i, 0), tf.int64)
+    y = first_round * pred + (1 - first_round) * y
+    one_hot = tf.one_hot(y, num_classes)
+
+    cross_entropy = tf.losses.softmax_cross_entropy(one_hot, logits_v3)
+    noise = tf.gradients(cross_entropy, x)[0]
+
+    noise = noise / tf.reduce_mean(tf.abs(noise), [1, 2, 3], keep_dims=True)
+
+    noise = momentum * grad + noise
+
+    x = x + alpha * tf.sign(noise)
+    x = tf.clip_by_value(x, x_min, x_max)
+    i = tf.add(i, 1)
+
+    return x, y, i, x_max, x_min, noise
+
+
+def stop(x, y, i, x_max, x_min, grad):
+    num_iter = FLAGS.num_iter
+    return tf.less(i, num_iter)
+
+
+def image_augmentation(x):
+    # img, noise
+    one = tf.fill([tf.shape(x)[0], 1], 1.)
+    zero = tf.fill([tf.shape(x)[0], 1], 0.)
+    transforms = tf.concat([one, zero, zero, zero, one, zero, zero, zero], axis=1)
+    rands = tf.concat([tf.truncated_normal([tf.shape(x)[0], 6], stddev=0.05), zero, zero], axis=1)
+    return images_transform(x, transforms + rands, interpolation='BILINEAR')
+
+
+def image_rotation(x):
+    """ imgs, scale, scale is in radians """
+    rands = tf.truncated_normal([tf.shape(x)[0]], stddev=0.05)
+    return images_rotate(x, rands, interpolation='BILINEAR')
+
+
+def input_diversity(input_tensor):
+    rnd = tf.random_uniform((), FLAGS.image_width, FLAGS.image_resize, dtype=tf.int32)
+    rescaled = tf.image.resize_images(input_tensor, [rnd, rnd], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    h_rem = FLAGS.image_resize - rnd
+    w_rem = FLAGS.image_resize - rnd
+    pad_top = tf.random_uniform((), 0, h_rem, dtype=tf.int32)
+    pad_bottom = h_rem - pad_top
+    pad_left = tf.random_uniform((), 0, w_rem, dtype=tf.int32)
+    pad_right = w_rem - pad_left
+    padded = tf.pad(rescaled, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], constant_values=0.)
+    padded.set_shape((input_tensor.shape[0], FLAGS.image_resize, FLAGS.image_resize, 3))
+    ret = tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+    ret = tf.image.resize_images(ret, [FLAGS.image_height, FLAGS.image_width],
+                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return ret
+    # return tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+
+def load_labels(file_name):
+    import pandas as pd
+    dev = pd.read_csv(file_name)
+    f2l = {dev.iloc[i]['filename']: dev.iloc[i]['label'] for i in range(len(dev))}
+    return f2l
+
